9b5300609678673e4c802ae02c297e92c4a06a2a
HamadKhanhehe <hamadkhann1010@gmail.com>
2026-01-11 19:36:24 +0500

    Fix: Add default export to TopRated page

---
+++ src/app/movie/top-rated/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TopRatedPage = () => {
+  return (
+    <div>
+      <h1>Top Rated Movies</h1>
+    </div>
+  );
+};
+
+export default TopRatedPage; // <-- Ye mandatory hai
